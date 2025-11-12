c38535fde5d4e7edc44c6a37a3ea22648ad1effb
Taviquenson <tavo2070@gmail.com>
2025-11-12 12:55:02 -0800

    refactor keyword search

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -3,6 +3,7 @@
 import argparse
 import json
 
+from lib.keyword_search import search_command
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Keyword Search CLI")
@@ -16,21 +17,9 @@ def main() -> None:
     match args.command:
         case "search":
             print("Searching for: " + args.query)
-            try:
-                with open("./data/movies.json", 'r') as file:
-                    searchDict = json.load(file)
-                results = [] # list of movie dicts
-                for movie in searchDict["movies"]:
-                    if (args.query) in movie["title"]:
-                        results.append(movie)
-                results.sort(key=lambda movie: movie['id']) #sorts in ascending order by id
-                for i in range(0,5):
-                    print(f"{str(i+1)}. {results[i]["title"]}")
-            except FileNotFoundError:
-                print("Error: 'config.json' not found.")
-            except json.JSONDecodeError:
-                print("Error: Invalid JSON format in 'config.json'.")
-            pass
+            results = search_command(args.query)
+            for i, res in enumerate(results, 1):
+                print(f"{i}. {res['title']}")
         case _:
             parser.print_help()
 
