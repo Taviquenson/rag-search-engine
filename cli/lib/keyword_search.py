3899c8e670ca6bf249654802c7aa1502fb56a051
Taviquenson <tavo2070@gmail.com>
2025-11-15 11:45:44 -0800

    doc length normalization

--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -10,6 +10,7 @@ from .search_utils import (
     CACHE_DIR,
     DEFAULT_SEARCH_LIMIT,
     BM25_K1,
+    BM25_B,
     load_movies,
     load_stopwords,
 )
@@ -21,9 +22,11 @@ class InvertedIndex:
                                       # Here, each new key gets a set() by default.
         self.docmap: dict[int, dict] = {} # dict of document IDs to movie objects
         self.term_frequencies = defaultdict(Counter) # Counter is a dictionary optimized for counting
+        self.doc_lengths = defaultdict(int)
         self.index_path = os.path.join(CACHE_DIR, "index.pkl")
         self.docmap_path = os.path.join(CACHE_DIR, "docmap.pkl")
         self.term_frequencies_path = os.path.join(CACHE_DIR, "term_frequencies.pkl")
+        self.doc_lengths_path = os.path.join(CACHE_DIR, "doc_lengths.pkl")
 
     def build(self) -> None:
         movies = load_movies()
@@ -41,6 +44,8 @@ class InvertedIndex:
             pickle.dump(self.docmap, f)
         with open(self.term_frequencies_path, "wb") as f:
             pickle.dump(self.term_frequencies, f)
+        with open(self.doc_lengths_path, "wb") as f:
+            pickle.dump(self.doc_lengths, f)
     
     def load(self):
         with open(self.index_path, "rb") as f:
@@ -49,6 +54,8 @@ class InvertedIndex:
             self.docmap = pickle.load(f)
         with open(self.term_frequencies_path, "rb") as f:
             self.term_frequencies = pickle.load(f)
+        with open(self.doc_lengths_path, "rb") as f:
+            self.doc_lengths = pickle.load(f)
 
     def get_documents(self, term: str) -> list[int]:
         doc_ids = self.index.get(term, set()) # Here, if term isn’t in the index, you get an empty set() instead of a KeyError.
@@ -67,6 +74,7 @@ class InvertedIndex:
         # Since Counter is built for routines like the above 4 lines of code
         # The same behaviour can be achieved with just this line of code:
         self.term_frequencies[doc_id].update(tokens) # Works like dict.update() but adds counts instead of replacing them
+        self.doc_lengths[doc_id] = len(tokens)
 
     def get_tf(self, doc_id: int, term: str) -> int:
         # Tokenize the term, but assume that there is only one token.
@@ -106,9 +114,26 @@ class InvertedIndex:
         term_doc_count = len(self.index[token])
         return math.log((doc_count - term_doc_count + 0.5) / (term_doc_count + 0.5) +1)
 
-    def get_bm25_tf(self, doc_id: int, term: str, k1 = BM25_K1):
+    def get_bm25_tf(self, doc_id: int, term: str, k1 = BM25_K1, b = BM25_B):
+        # Length normalization factor
+        doc_length = self.doc_lengths.get(doc_id, 0)
+        avg_doc_length = self.__get_avg_doc_length()
+        if avg_doc_length > 0:
+            length_norm = 1 - b + b * (doc_length / avg_doc_length)
+        else:
+            length_norm = 1
+        # Apply to term frequency
         tf = self.get_tf(doc_id, term)
-        return (tf * (k1 + 1)) / (tf + k1)
+        return (tf * (k1 + 1)) / (tf + k1 * length_norm)
+    
+    def __get_avg_doc_length(self) -> float:
+        if not self.doc_lengths or len(self.doc_lengths) == 0:
+            return 0.0
+        total_docs_length = 0
+        for doc_length in self.doc_lengths.values(): # REMEMBER, without .values() the keys are returned!
+            total_docs_length += doc_length
+        return float(total_docs_length / len(self.doc_lengths))
+        
 
 def build_command() -> None:
     idx = InvertedIndex()
@@ -154,10 +179,10 @@ def bm25_idf_command(term: str) -> float:
     idx.load()
     return idx.get_bm25_idf(term)
 
-def bm25_tf_command(doc_id: int, term: str, k1 = BM25_K1) -> float:
+def bm25_tf_command(doc_id: int, term: str, k1 = BM25_K1, b = BM25_B) -> float:
     idx = InvertedIndex()
     idx.load()
-    return idx.get_bm25_tf(doc_id, term, k1) 
+    return idx.get_bm25_tf(doc_id, term, k1, b) 
 
 
 def has_matching_token(query_tokens: list[str], title_tokens: list[str]) -> bool:
